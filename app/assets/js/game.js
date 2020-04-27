7c6e50ccf8579df0dcf663a032553818c9e85faa
sctlcd <lucinda.souchet@outlook.com>
2020-04-27 23:51:32 +0000

    add checkForSuccess function, update layout and style

--- app/assets/js/game.js
+++ app/assets/js/game.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
         init: function() {
             game.shuffle();
         },
-        // Iterate through the array Card and each time generate a random number 
-        // then swapping numbers which going to shuffle cards
+        // Iterate over the cards array and each time generate a random number 
+        // then swapping numbers which shuffles cards and then, after cards are shuffled, assign cards
         shuffle: function() {
             var random = 0;
             var temp = 0;
@@ -16,9 +16,47 @@ $(document).ready(function() {
                 game.cards[i] = game.cards[random];
                 game.cards[random] = temp;
             }
-            console.log(game.cards);
+            game.getCards();
+        },
+        // Iterate over each one on the elements with class="card" and assign a data-value attribute and a relative value to each one of the individual cards 
+        // https://api.jquery.com/data/
+        getCards: function() {
+            $(".card").each(function (index) {
+                $(this).attr("data-card-figure", game.cards[index]);
+            });
+            game.clickCardHandlers();
+        },
+        // Display the figure of the card on-click and add the class attibute 'selected' to the selected card.
+        clickCardHandlers: function() {
+            $(".card").click (function() {
+                $(this).html("<p>" + $(this).data("cardFigure") + "</p>").addClass("visible");
+                game.checkForPairedCards();
+            });
+        },
+        checkForPairedCards: function() {
+            if ($(".visible").length == 2) {
+                if($(".visible").first().data("cardFigure") == $(".visible").last().data("cardFigure")) {
+                    $(".visible").each(function() {
+                        $(this).hide().removeClass("unpaired");
+                    });
+                    $(".visible").each( function() {
+                        $(this).removeClass("visible");
+                    });
+                    game.checkForSuccess();
+                } else {
+                    setTimeout(function() {
+                        $(".visible").each(function() {
+                            $(this).html("").removeClass("visible");
+                        });
+                    }, 1000);
+                }
+            }
+        },
+        checkForSuccess: function() {
+            if($("unpaired").length == 0) {
+                $(".container-cards").html('<h1> Congrats! You Won! :) </h1>');
+            }
         }
     };
-
     game.init();
 });
