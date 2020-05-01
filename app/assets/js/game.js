34e934a25be3d5e9b61f4d83d2579839906efc12
sctlcd <lucinda.souchet@outlook.com>
2020-05-01 15:37:22 +0000

    small style updates (background image, performanace badges/button)

--- app/assets/js/game.js
+++ app/assets/js/game.js
@@ -1,148 +1,161 @@
-$(document).ready(function() {
-    
-    // game namespace/game variable  
-    var game = {
-        cards: [1,2,3,4,5,6,1,2,3,4,5,6],
-        playerName,
-        moves: 0,
-        timeCounter: 0,
-        imgCategory: "any",
-        init: function() {   
-            $(".header-details").append("<h1>Memory game</h1>").addClass("uppercase");  
-            $("#startGameButton").click(function(){
-            game.getPlayerData();            
-            });
-        },
+$(document).ready(function () {
+  // game namespace/game variable
+  var game = {
+    cardFigures: ["A", "B", "C", "D", "E", "F", "A", "B", "C", "D", "E", "F"],
+    playerName,
+    moves: 0,
+    timeCounter: 0,
+    imgCategory: "any",
+    init: function () {
+      $(".header-details").append("<h1>Memory game</h1>").addClass("uppercase");
+      $("#startGameButton").click(function () {
+        game.getPlayerData();
+      });
+    },
 
-        getPlayerData: function() {
-            if ($("#playerName").val() !== "") {
-                game.playerName = $("#playerName").val();
-                game.clickCardHandlers();                
-                $(".performance-details").empty();
-                $(".performance-details").append('<p><h4 class="inline"><span class="badge badge-primary moves"><span id="moves">0</span> moves</span><span class="badge badge-primary seconds"><span id="sec_timer">0</span> s</span></h4><button type="button" class="btn btn btn-primary" id="restart"><i class="fas fa-redo-alt"></i></button></p>'); 
-                //  <span class="badge badge-primary"><a id="restart"><i class="fas fa-redo-alt"></i></a></span>
-                game.restart();
-                game.initTime();
-                game.resetMoves();
-                game.shuffleCards();
-                game.preloadImages();
-                $("#startModal").modal("toggle");
-            }else {
-                setTimeout(function() {
-                    $("#playerName").effect("bounce");
-                },500);      
-            } 
-        },
+    getPlayerData: function () {
+      if ($("#playerName").val() !== "") {
+        game.playerName = $("#playerName").val();
+        game.clickCardHandlers();
+        $(".performance-details").empty();
+        $(".performance-details").append(
+          '<p><h4 class="inline"><span class="badge badge-primary moves"><span id="moves">0</span> moves</span><span class="badge badge-primary seconds"><span id="sec_timer">0</span> s</span></h4><button type="button" class="btn btn btn-primary" id="restart"><i class="fas fa-redo-alt"></i></button></p>'
+        );
+        //  <span class="badge badge-primary"><a id="restart"><i class="fas fa-redo-alt"></i></a></span>
+        game.restart();
+        game.initTime();
+        game.resetMoves();
+        game.shuffleCards();
+        game.preloadImages();
+        $("#startModal").modal("toggle");
+      } else {
+        setTimeout(function () {
+          $("#playerName").effect("bounce");
+        }, 1000);
+      }
+    },
 
-        // Iterate over the cards array and each time generate a random number 
-        // then switching numbers which shuffles cards and add the html element relative to each individual card with the classes card and unpaired
-        // and then, after cards are shuffled, assign cards
-        shuffleCards: function() {
-            var switching = 0;
-            var temp = 0;
-            $(".card-details").empty();
-            for ( i = 0; i < game.cards.length ; i++) {
-                switching = Math.round(Math.random() * i);
-                temp = game.cards[i];
-                game.cards[i] = game.cards[switching];
-                game.cards[switching] = temp;
-                $(".card-details").append('<div class="card unpaired"></div>');
-            }
-            game.getCards();
-        },
+    // Iterate over the cards array and each time generate a random number
+    // then switching numbers which shuffles cards and add the html element relative to each individual card with the classes card and unpaired
+    // and then, after cards are shuffled, assign cards
+    shuffleCards: function () {
+      var switching = 0;
+      var temp = 0;
+      $(".card-details").empty();
+      for (i = 0; i < game.cardFigures.length; i++) {
+        switching = Math.round(Math.random() * i);
+        temp = game.cardFigures[i];
+        game.cardFigures[i] = game.cardFigures[switching];
+        game.cardFigures[switching] = temp;
+        $(".card-details").append('<div class="card unpaired"></div>');
+      }
+      game.getCards();
+    },
 
-        // Iterate over each one on the elements with class="card" and assign a data-value attribute and a relative value to each one of the individual cards 
-        // https://api.jquery.com/data/
-        getCards: function() {
-            $(".card").each(function (index) {
-                $(this).attr("data-card-figure", game.cards[index]);
-            });
-            game.clickCardHandlers();
-        },
+    // Iterate over each one on the elements with class="card" and assign a data-value attribute and a relative value to each one of the individual cards
+    // https://api.jquery.com/data/
+    getCards: function () {
+      $(".card").each(function (index) {
+        $(this).attr("data-card-figure", game.cardFigures[index]);
+      });
+      game.clickCardHandlers();
+    },
 
-        clickCardHandlers: function() {
-            $(".card").click (function() {
-                game.flipCard($(this));           
-            });
-        },
+    clickCardHandlers: function () {
+      $(".card").click(function () {
+        game.flipCard($(this));
+      });
+    },
 
-        // On-click flip the card and display the figure of the card
-        flipCard: function(card) {
-            card.addClass("visible");
-            card.css({'background-image': 'url("https://placeimg.com/150/150/'+game.imgCategory+'/'+card.data("cardFigure")+'")'});
-			if($(".visible").length == 2) {
-				game.checkForPairedCards();
-			}
-        },
+    // On-click flip the card and display the figure of the card
+    flipCard: function (card) {
+      card.addClass("visible");
+      card.css({
+        "background-image":
+          'url("https://placeimg.com/150/150/' +
+          game.imgCategory +
+          "/" +
+          card.data("cardFigure") +
+          '")',
+      });
+      if ($(".visible").length == 2) {
+        game.checkForPairedCards();
+      }
+    },
 
-        // Check if a pair of cards have a similar figure, make the paired ones disapeared and hide the figures of the unpaired ones after a timeout interval
-        checkForPairedCards: function() {
-            if($(".visible").first().data("cardFigure") == $(".visible").last().data("cardFigure")) {
-                setTimeout(function() {
-                    $(".visible").each(function() {
-                        $(this).css({opacity:0},{duration:1000}).removeClass("unpaired visible");
-                    });
-                    game.checkForSuccess();
-                }, 500);
-            } else {
-                setTimeout(function() {
-                    $(".visible").each(function() {
-                        $(this).css({"background":""}).removeClass("visible");
-                    });
-                }, 500);
-            }
-            game.moves++;
-            $("#moves").html("" + game.moves);
-        },
+    // Check if a pair of cards have a similar figure, make the paired ones disapeared and hide the figures of the unpaired ones after a timeout interval
+    checkForPairedCards: function () {
+      if (
+        $(".visible").first().data("cardFigure") ==
+        $(".visible").last().data("cardFigure")
+      ) {
+        setTimeout(function () {
+          $(".visible").each(function () {
+            $(this)
+              .css({ opacity: 0 }, { duration: 1000 })
+              .removeClass("unpaired visible");
+          });
+          game.checkForSuccess();
+        }, 1000);
+      } else {
+        setTimeout(function () {
+          $(".visible").each(function () {
+            $(this).css({ background: "" }).removeClass("visible");
+          });
+        }, 1000);
+      }
+      game.moves++;
+      $("#moves").html("" + game.moves);
+    },
 
-        // Check if all cards are paired
-        checkForSuccess: function() {
-            if($(".unpaired").length === 0) {
-                $(".container-cards").html("<h1> Congrats! You Won! :) </h1>");
-            }
-        },
+    // Check if all cards are paired
+    checkForSuccess: function () {
+      if ($(".unpaired").length === 0) {
+        $(".container-cards").html("<h1> Congrats! You Won! :) </h1>");
+      }
+    },
 
-        resetMoves: function() {
-            game.moves = 0;
-            $("#moves").html("" + game.moves);
-        },
+    resetMoves: function () {
+      game.moves = 0;
+      $("#moves").html("" + game.moves);
+    },
 
-        resetTime: function() {
-            game.timeCounter = -1;
-            $("#sec_timer").html("", game.timeCounter);
-        },
+    resetTime: function () {
+      game.timeCounter = -1;
+      $("#sec_timer").html("", game.timeCounter);
+    },
 
-        restart: function() {
-            $("#restart").click(function() {
-                game.resetMoves();
-                game.resetTime();
-                game.shuffleCards();
-            })
-        },
-            
-        initTime: function() {
-            setInterval(function () {
-                game.timeCounter++;      
-                 $("#sec_timer").html("" + game.timeCounter);
-            }, 1000);   
-        }, 
+    restart: function () {
+      $("#restart").click(function () {
+        game.resetMoves();
+        game.resetTime();
+        game.shuffleCards();
+      });
+    },
 
-        preloadImages: function() {
-            var img = [];
-            for (i = 0; i < game.cards.length; i++) {
-                img[i] = new Image;
-                img[i].src = "https://placeimg.com/150/150/"+game.imgCategory+"/"+i;
-            }
-        }
-    };
+    initTime: function () {
+      setInterval(function () {
+        game.timeCounter++;
+        $("#sec_timer").html("" + game.timeCounter);
+      }, 1000);
+    },
 
-    $('#btnStartModal').click(function(){
-  		$('#startModal').modal({
-    		backdrop: 'static',
-    		keyboard: false
-        });
-        $(this).hide();
-	});
-    game.init();
-    
+    preloadImages: function () {
+      var img = [];
+      for (i = 0; i < game.cardFigures.length; i++) {
+        img[i] = new Image();
+        img[i].src =
+          "https://placeimg.com/150/150/" + game.imgCategory + "/" + i;
+      }
+    },
+  };
+
+  $("#btnStartModal").click(function () {
+    $("#startModal").modal({
+      backdrop: "static",
+      keyboard: false,
+    });
+    $(this).hide();
+  });
+  game.init();
 });
